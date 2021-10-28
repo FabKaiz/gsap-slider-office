13329eea0d1553d37d38e8334723206e30d22891
Fabien <cacrfabfr@gmail.com>
2021-10-28 11:08:31 +0200

    refactor the animation to make it reusable

--- js/main.js
+++ js/main.js
@@ -2,40 +2,58 @@ function init(){
   gsap.set('.projects', {autoAlpha: 1});
   gsap.set('.project', {x: '-100%'});
 
-  const element        = document.querySelector('div.project01'),
-        projectClasses = element.className.split(' '),
-        projectClass   = projectClasses[1];
+  let currentStep = 3;
 
   // Update de class of the body to set the background color
   const updateClass = (projectClass) => {
-          document.querySelector('body').className = projectClass;
-        }
+    document.querySelector('body').className = projectClass;
+  }
 
   // Animation In
-  const tlIn = gsap.timeline();
-  tlIn.fromTo(element, {
-    autoAlpha: 0,
-    x: '-100%',
-  }, {
-    duration: 0.7, x: 0,autoAlpha: 1,
-    onStart: updateClass,
-    onStartParams: [projectClass]
-  })
+  const createTimelineIn = (index) => {
+    const element        = document.querySelector('div.project0' + index),
+          projectClasses = element.className.split(' '),
+          projectClass   = projectClasses[1];
+
+    const tlIn = gsap.timeline();
+    tlIn.fromTo(element, {
+      autoAlpha: 0,
+      x: '-100%',
+    }, {
+      duration: 0.7, x: 0,autoAlpha: 1,
+      onStart: updateClass,
+      onStartParams: [projectClass]
+    });
+
+    return tlIn;
+  }
 
   // Animation Out
-  const tlOut = gsap.timeline();
-  tlOut.to(element, {
-    duration: 0.7,
-    x: 250,
-    autoAlpha: 0
-  })
+  const createTimelineOut = (index) => {
+    const tlOut = gsap.timeline();
+    tlOut.to(element, {
+      duration: 0.7,
+      x: 250,
+      autoAlpha: 0
+    })
 
-  // Timeline transition for in and out animation
-  const tlTransition = gsap.timeline();
-  tlTransition
-    .add(tlIn)
-    .add(tlOut)
-}
+    return tlOut;
+
+  }
+
+  const transition = () => {
+    // Timeline transition for in and out animation
+    const tlTransition = gsap.timeline();
+    tlTransition
+      .add(tlIn)
+      .add(tlOut)
+
+    return tlTransition;
+  }
+
+  createTimelineIn(currentStep);
+  }
+
 
 window.addEventListener('load', function(){
     init();
