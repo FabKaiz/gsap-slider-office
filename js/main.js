ea5fa166baae9ff6137dfbac51ec5d1d27004a50
Fabien <cacrfabfr@gmail.com>
2021-10-28 12:24:39 +0200

    set default params for transition

--- js/main.js
+++ js/main.js
@@ -17,9 +17,21 @@ function init(){
 
     const element        = document.querySelector('div.project0' + index),
           projectClasses = element.className.split(' '),
-          projectClass   = projectClasses[1];
+          projectClass   = projectClasses[1],
+          title          = element.querySelector('.project-title'),
+          subtitle       = element.querySelector('.project-subtitle'),
+          button         = element.querySelector('.button-container');
 
-    const tlIn = gsap.timeline();
+    const tlIn = gsap.timeline({ 
+      defaults: {
+        modifiers: {
+          x: gsap.utils.unitize(function(x) {
+            return goPrev ? Math.abs(x) : x;
+          })
+        }
+      }
+    });
+
     tlIn.fromTo(element, {
       autoAlpha: 0,
       x: '-100%',
@@ -27,11 +39,14 @@ function init(){
       duration: 0.7, x: 0,autoAlpha: 1,
       onStart: updateClass,
       onStartParams: [projectClass],
-      modifiers: {
-        x: gsap.utils.unitize(function(x) {
-          return goPrev ? Math.abs(x) : x;
-        })
-      }
+
+    })
+    .from([title, subtitle, button], {
+      duration: 0.8,
+      x: -20,
+      autoAlpha: 0,
+      stagger: 0.6,
+
     });
 
     return tlIn;
@@ -52,7 +67,8 @@ function init(){
         x: gsap.utils.unitize(function(x) {
           return goPrev ? -x : x;
         })
-      }
+      },
+      ease: "back.in(2)",
     })
 
     return tlOut;
