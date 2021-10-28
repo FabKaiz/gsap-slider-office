fbcb52e4ef054581202e1fddb2631d9939a8f052
Fabien <cacrfabfr@gmail.com>
2021-10-28 10:58:35 +0200

    added gsap animation timeline in and out

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
 function init(){
+  gsap.set('.projects', {autoAlpha: 1});
+  gsap.set('.project', {x: '-100%'});
 
-    // welcome to the projects gallery
+  const element        = document.querySelector('div.project01'),
+        projectClasses = element.className.split(' '),
+        projectClass   = projectClasses[1];
 
+  // Update de class of the body to set the background color
+  const updateClass = (projectClass) => {
+          document.querySelector('body').className = projectClass;
+        }
+
+  // Animation In
+  const tlIn = gsap.timeline();
+  tlIn.fromTo(element, {
+    autoAlpha: 0,
+    x: '-100%',
+  }, {
+    duration: 0.7, x: 0,autoAlpha: 1,
+    onStart: updateClass,
+    onStartParams: [projectClass]
+  })
+
+  // Animation Out
+  const tlOut = gsap.timeline();
+  tlOut.to(element, {
+    duration: 0.7,
+    x: 250,
+    autoAlpha: 0
+  })
+
+  // Timeline transition for in and out animation
+  const tlTransition = gsap.timeline();
+  tlTransition
+    .add(tlIn)
+    .add(tlOut)
 }
 
 window.addEventListener('load', function(){
